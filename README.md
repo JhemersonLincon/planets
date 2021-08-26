2d5e1db5334b3d069e35a8154aa08f51915b6af1
Lincon <jhemersonlincon003@gmail.com>
2021-08-26 19:29:05 -0300

    botôes adcionados

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 [x]pagina inicial index para abertura das outras entradas
 [X]Deixa o inicial responsivo para outros dispositivos
 [X]Criar os planetas
-[]Revisar planetas
+[X]Revisar planetas
+[X]Criar botões personalizados
--- js/button-mobile.js
+++ js/button-mobile.js
@@ -1,12 +1,25 @@
-const btn = document.querySelector("button#btn-mobile");
+const btn_mobile = document.querySelector("button#btn-mobile");
 const nav = document.querySelector("#nav");
+const area = document.querySelector("div.ações")
 
 
 //carregando botão
-btn.addEventListener("click", e =>{
+btn_mobile.addEventListener("click", e =>{
     nav.classList.toggle("active");
+    area.classList.toggle("hidden")
 })
 
 
 
-
+// carrega e apaga os botôes das tag btn
+const btns = document.querySelectorAll(".btn");
+btns.forEach(Element =>{
+    
+    Element.addEventListener("click", function(){
+        btns.forEach(elements =>{
+            elements.classList.remove("selected")
+        })
+        this.classList.add("selected")
+        
+    })
+})
